4ac3baf31b28151d0b53ff8d1b4f4230c7aa2b36
parsher <futureslabor@gmail.com>
2019-10-25 18:18:55 +0900

    vuetify dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import ('../views/Login.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import ('../views/Dashboard.vue')
   }
+
 ]
 
 const router = new VueRouter({
